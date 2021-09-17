a4cebf196b59e893c948194f444b54d64206937d
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2021-09-17 14:52:41 +0900

    installed Retrofit and Moshi library to access and parse data from API

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
@@ -8,19 +8,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import jp.co.yumemi.android.code_check.MainActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.code_check.model.GitHubRepository
+import jp.co.yumemi.android.code_check.network.GitHubApi
 import kotlinx.coroutines.launch
-import org.json.JSONObject
 import java.lang.Exception
 import java.util.*
-
-const val API_URL = "https://api.github.com"
 
 enum class ApiStatus {
     LOADING,
@@ -41,43 +34,12 @@ class SearchViewModel : ViewModel() {
 
 
     fun searchRepositories(inputText: String) {
+        Log.d("ViewModel", "search")
         viewModelScope.launch {
             _apiStatus.value = ApiStatus.LOADING
 
             try {
-                val httpClient = HttpClient(Android)
-                val response: HttpResponse =
-                    httpClient.get("${API_URL}/search/repositories") {
-                        header("Accept", "application/vnd.github.v3+json")
-                        parameter("q", inputText)
-                    }
-
-                httpClient.close()
-
-                val jsonBody = JSONObject(response.receive<String>())
-                val jsonItems = jsonBody.optJSONArray("items")
-
-                val items = mutableListOf<GitHubRepository>()
-
-                for (i in 0 until jsonItems.length()) {
-                    val jsonItem = jsonItems.optJSONObject(i) ?: return@launch
-
-                    val gitHubRepository = GitHubRepository(
-                        name = jsonItem.optString("full_name"),
-                        ownerIconUrl = (jsonItem.optJSONObject("owner") ?: return@launch).optString(
-                            "avatar_url"
-                        ),
-                        language = jsonItem.optString("language"),
-                        stargazersCount = jsonItem.optLong("stargazers_count"),
-                        watchersCount = jsonItem.optLong("watchers_count"),
-                        forksCount = jsonItem.optLong("forks_count"),
-                        openIssuesCount = jsonItem.optLong("open_issues_count")
-                    )
-
-                    items.add(gitHubRepository)
-                }
-
-                _gitHubRepositories.value = items.toList()
+                _gitHubRepositories.value = GitHubApi.retrofitService.searchRepos(inputText).items
 
                 lastSearchDate = Date()
 
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/model/GitHubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/GitHubRepository.kt
@@ -1,15 +1,25 @@
 package jp.co.yumemi.android.code_check.model
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
+
+data class ReposResponse(
+    val items: List<GitHubRepository>
+)
 
 @Parcelize
 data class GitHubRepository(
     val name: String,
-    val ownerIconUrl: String,
-    val language: String,
-    val stargazersCount: Long,
-    val watchersCount: Long,
-    val forksCount: Long,
-    val openIssuesCount: Long,
-) : Parcelable
+    val owner: RepositoryOwner,
+    val language: String?,
+    @Json(name = "stargazers_count") val stargazersCount: Long,
+    @Json(name = "watchers_count") val watchersCount: Long,
+    @Json(name = "forks_count") val forksCount: Long,
+    @Json(name = "open_issues_count") val openIssuesCount: Long
+) : Parcelable {
+    @Parcelize
+    data class RepositoryOwner(
+        @Json(name = "avatar_url") val iconUrl: String
+    ) : Parcelable
+}
